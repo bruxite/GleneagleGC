96233532e04e66841122f71409ad5513557fff62
bruxite <mike.bruxvoort@gmail.com>
2026-01-01 16:30:09 -0500

    Converted the Images to Unity formats

--- Lidar/QGIS/Heightmap/OUTER/OUTER-TIFtoRAW-Heightmap.bat
+++ Lidar/QGIS/Heightmap/OUTER/OUTER-TIFtoRAW-Heightmap.bat
@@ -1,5 +1,5 @@
 for %%A IN (*.tif) do (
-"C:\Program Files\QGIS 3.28.14\bin\gdal_translate.exe" -of ENVI -ot UInt16 -scale MIN MAX 0 65535 -outsize 4097 4097 -r bilinear "%%A" "%%~nA".raw
+"C:\Program Files\QGIS 3.28.14\bin\gdal_translate.exe" -of ENVI -ot UInt16 -scale 599.527 726.69 0 65535 -outsize 4097 4097 -r bilinear "%%A" "%%~nA".raw
 )
 
  @echo off    
